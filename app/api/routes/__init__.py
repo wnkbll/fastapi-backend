14c178834ca7b263aabaff0bef814cbd5fefa0c0
wnkbll <arzhevitin.g@gmail.com>
2024-06-29 16:11:57 +0500

    Add authentication router

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import users
+from app.api.routes import users, authentication
 
 router = APIRouter()
 
-router.include_router(users.router, tags=["users"], prefix="/users")
+router.include_router(users.router, tags=["users"], prefix="/user")
+router.include_router(authentication.router, tags=["authentication"], prefix="/users")
 
 __all__ = [
     "router",
