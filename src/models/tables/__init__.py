2427f35f0cc762a7174aaa886682259df0a6b49a
wnkbll <arzhevitin.g@gmail.com>
2024-07-09 13:33:04 +0500

    Move Table class to __init__

--- src/models/tables/__init__.py
+++ src/models/tables/__init__.py
@@ -1,7 +1,21 @@
-from sqlalchemy import Integer, String, ForeignKey, Text
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import MetaData, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-from src.models.tables.table import Table
+
+class Table(DeclarativeBase):
+    convention = {
+        "all_column_names": lambda constraint, table: "_".join([
+            column.name for column in constraint.columns.values()
+        ]),
+        "ix": "ix__%(table_name)s__%(all_column_names)s",
+        "uq": "uq__%(table_name)s__%(all_column_names)s",
+        "ck": "ck__%(table_name)s__%(constraint_name)s",
+        "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
+        "pk": "pk__%(table_name)s",
+    }
+
+    metadata = MetaData()
+    metadata.naming_convention = convention
 
 
 class UsersTable(Table):
