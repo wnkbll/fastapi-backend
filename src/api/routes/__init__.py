8de1acb4d268d70f0d19b7729d0293b00de5095c
wnkbll <arzhevitin.g@gmail.com>
2024-07-11 13:48:44 +0500

    Add jwt service

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from src.api.routes import default, articles
+from src.api.routes import default, articles, authentication
 
 router = APIRouter()
 
 router.include_router(default.router, tags=["Default"], prefix="/default")
 router.include_router(articles.router, tags=["Articles"], prefix="/articles")
+router.include_router(authentication.router, tags=["Auth"], prefix="/auth")
 
 __all__ = [
     "router",
--- src/api/routes/authentication.py
+++ src/api/routes/authentication.py
@@ -1,12 +1,22 @@
-from fastapi import APIRouter
-from src.models.schemas.users import UserInResponse
+from fastapi import APIRouter, Body
+
 from src.api.dependencies import UsersRepositoryDepends
+from src.models.schemas.users import UserInResponse, UserInLogin, UserInCreate
 
 router = APIRouter()
 
 
 @router.post("", name="auth:login", response_model=UserInResponse)
 async def login(
-        user_repo: UsersRepositoryDepends
+        user_repo: UsersRepositoryDepends,
+        user_in_login: UserInLogin = Body(),
+) -> UserInResponse:
+    pass
+
+
+@router.post("", name="auth:register", response_model=UserInResponse)
+async def register(
+        user_repo: UsersRepositoryDepends,
+        user_in_create: UserInCreate = Body(alias="user-in-create"),
 ) -> UserInResponse:
     pass
