4312855f7441eca74dbd8078ec977b985fb84eda
wnkbll <arzhevitin.g@gmail.com>
2024-07-17 16:51:54 +0500

    Refactoring

--- src/main.py
+++ src/main.py
@@ -16,38 +16,6 @@ from src.core.events import create_start_app_handler, create_stop_app_handler
 from src.core.paths import LOGGING_DIR
 
 
-def configure_logging(settings: Environment) -> None:
-    logger.configure(
-        handlers=[
-            dict(
-                sink=LOGGING_DIR.joinpath(settings.logging.file),
-                level="WARNING",
-                rotation=settings.logging.rotation,
-                compression=settings.logging.compression,
-                serialize=True,
-            ),
-            dict(
-                sink=sys.stderr,
-                level="TRACE",
-            ),
-        ],
-    )
-
-
-def register_middleware(application: FastAPI, settings: Environment) -> None:
-    application.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.middleware.allow_origins,
-        allow_credentials=settings.middleware.allow_credentials,
-        allow_methods=settings.middleware.allow_methods,
-        allow_headers=settings.middleware.allow_headers,
-    )
-
-
-def register_routers(application: FastAPI, settings: Environment) -> None:
-    application.include_router(router, prefix=settings.api_prefix)
-
-
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     await create_start_app_handler()
@@ -55,21 +23,37 @@ async def lifespan(_: FastAPI):
     await create_stop_app_handler()
 
 
-def get_application(env_type: EnvironmentTypes) -> FastAPI:
-    settings: Environment = get_app_settings(env_type)
-    configure_logging(settings)
+settings: Environment = get_app_settings(EnvironmentTypes.dev)
 
-    application: FastAPI = FastAPI(**settings.fastapi_kwargs.model_dump(), lifespan=lifespan)
+logger.configure(
+    handlers=[
+        dict(
+            sink=LOGGING_DIR.joinpath(settings.logging.file),
+            level="WARNING",
+            rotation=settings.logging.rotation,
+            compression=settings.logging.compression,
+            serialize=True,
+        ),
+        dict(
+            sink=sys.stderr,
+            level="TRACE",
+        ),
+    ],
+)
 
-    register_middleware(application, settings)
-    register_routers(application, settings)
+app: FastAPI = FastAPI(
+    **settings.fastapi_kwargs.model_dump(),
+    lifespan=lifespan,
+)
 
-    logger.success("Application was successfully initialized")
-
-    return application
-
-
-app: FastAPI = get_application(EnvironmentTypes.dev)
+app.include_router(router, prefix=settings.api_prefix)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.middleware.allow_origins,
+    allow_credentials=settings.middleware.allow_credentials,
+    allow_methods=settings.middleware.allow_methods,
+    allow_headers=settings.middleware.allow_headers,
+)
 
 
 @app.exception_handler(HTTPException)
