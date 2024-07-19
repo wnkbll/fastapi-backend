9eb564e5387837f0afe114757a9ee147c18d2ede
wnkbll <arzhevitin.g@gmail.com>
2024-07-19 20:25:34 +0500

    Add endpoints to tasks router

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
-from src.api.routes import default, articles, authentication
+from src.api.routes import default, tasks, auth
 
 router = APIRouter()
 
 router.include_router(default.router, tags=["Default"], prefix="/default")
-router.include_router(articles.router, tags=["Articles"], prefix="/articles")
-router.include_router(authentication.router, tags=["Auth"], prefix="/auth")
+router.include_router(tasks.router, tags=["Tasks"], prefix="/tasks")
+router.include_router(auth.router, tags=["Auth"], prefix="/auth")
 
 __all__ = [
     "router",
--- src/api/routes/tasks.py
+++ src/api/routes/tasks.py
@@ -1,3 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+
+from src.db.errors import EntityDoesNotExistError
+from src.models.schemas.tasks import TasksInResponse
+from src.core.dependencies import TasksRepositoryDepends
 
 router = APIRouter()
+
+
+@router.get(
+    "/", name="tasks:get-all-tasks", response_model=TasksInResponse
+)
+async def get_all_tasks_(
+        tasks_repo: TasksRepositoryDepends
+) -> TasksInResponse:
+    try:
+        tasks = await tasks_repo.get_all()
+    except EntityDoesNotExistError as existence_error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid username",
+        ) from existence_error
+
+    return TasksInResponse(
+        tasks=tasks,
+        count=len(tasks),
+    )
+
+
+@router.get(
+    "/{username}", name="tasks:get-all-tasks-by-username", response_model=TasksInResponse
+)
+async def get_all_tasks_by_username(
+        username: str, tasks_repo: TasksRepositoryDepends
+) -> TasksInResponse:
+    try:
+        tasks = await tasks_repo.get_all(username=username)
+    except EntityDoesNotExistError as existence_error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid username",
+        ) from existence_error
+
+    return TasksInResponse(
+        tasks=tasks,
+        count=len(tasks),
+    )
