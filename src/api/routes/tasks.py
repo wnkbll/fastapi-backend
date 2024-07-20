3b412cc986ab32b89e25734ebeb4a627c8e35963
wnkbll <arzhevitin.g@gmail.com>
2024-07-20 10:39:06 +0500

    Implement tasks router

--- src/api/routes/tasks.py
+++ src/api/routes/tasks.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter, HTTPException, status
+from typing import Annotated
 
-from src.db.errors import EntityDoesNotExistError
-from src.models.schemas.tasks import TasksInResponse
+from fastapi import APIRouter, HTTPException, status, Body
+
 from src.core.dependencies import TasksRepositoryDepends
+from src.db.errors import EntityDoesNotExistError
+from src.models.schemas.tasks import TaskInResponse, TasksInResponse, TaskInCreate, TaskInUpdate
 
 router = APIRouter()
 
 
 @router.get(
-    "/", name="tasks:get-all-tasks", response_model=TasksInResponse
+    "", name="tasks:get-all-tasks", response_model=TasksInResponse
 )
 async def get_all_tasks_(
         tasks_repo: TasksRepositoryDepends
@@ -44,4 +46,65 @@ async def get_all_tasks_by_username(
     return TasksInResponse(
         tasks=tasks,
         count=len(tasks),
+    )
+
+
+@router.post(
+    "", name="tasks:create-task", response_model=TaskInResponse
+)
+async def create_task(
+        tasks_repo: TasksRepositoryDepends,
+        task_in_create: Annotated[TaskInCreate, Body(alias="task-in-create")],
+) -> TaskInResponse:
+    try:
+        task = await tasks_repo.create(task_in_create=task_in_create)
+    except EntityDoesNotExistError as existence_error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid username",
+        ) from existence_error
+
+    return TaskInResponse(
+        task=task
+    )
+
+
+@router.put(
+    "/{id: int}", name="task:update-task", response_model=TaskInResponse
+)
+async def update_task(
+        id_: int,
+        tasks_repo: TasksRepositoryDepends,
+        task_in_update: Annotated[TaskInUpdate, Body(alias="task-in-update")],
+) -> TaskInResponse:
+    try:
+        task = await tasks_repo.update(id_=id_, task_in_update=task_in_update)
+    except EntityDoesNotExistError as existence_error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid id",
+        ) from existence_error
+
+    return TaskInResponse(
+        task=task
+    )
+
+
+@router.delete(
+    "/{id: int}", name="task:delete-task", response_model=TaskInResponse
+)
+async def update_task(
+        id_: int,
+        tasks_repo: TasksRepositoryDepends,
+) -> TaskInResponse:
+    try:
+        task = await tasks_repo.delete(id_=id_)
+    except EntityDoesNotExistError as existence_error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid id",
+        ) from existence_error
+
+    return TaskInResponse(
+        task=task
     )
