c8a4e242e45049dbae09e3068feca2d8f8b47f2f
wnkbll <arzhevitin.g@gmail.com>
2024-07-30 01:27:32 +0500

    Remove PROD_ENV_PATH

--- src/core/paths.py
+++ src/core/paths.py
@@ -3,6 +3,5 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.parent.parent
 SRC_DIR = ROOT_DIR.joinpath("src")
 
-DEV_ENV_PATH = ROOT_DIR.joinpath(".env")
-PROD_ENV_PATH = ROOT_DIR.joinpath(".env.prod")
+ENV_PATH = ROOT_DIR.joinpath(".env")
 LOGGING_DIR = ROOT_DIR.joinpath("logs/")
